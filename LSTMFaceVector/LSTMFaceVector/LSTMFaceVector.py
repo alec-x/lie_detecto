4b1dc01793ef6155b163c5dc9f6904229280f024
Alec Xu <Alec_Xu@hotmail.com>
2018-12-01 14:43:11 -0800

    create 3D numpy array to feed into LSTM model as well as implement model as per https://machinelearningmastery.com/5-step-life-cycle-long-short-term-memory-models-keras/

--- LSTMFaceVector/LSTMFaceVector/LSTMFaceVector.py
+++ LSTMFaceVector/LSTMFaceVector/LSTMFaceVector.py
@@ -1,6 +1,32 @@
 import os
+import pandas
+import glob
+import numpy
+from pandas import DataFrame
+from pandas import concat
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Activation
 
-print("hello world")
+def main():
+    path = 'C:\Repos\mech423final\LSTMFaceVector\LSTMFaceVector'
+    i = 0
+    for filename in glob.glob(os.path.join(path, '*.txt')):
+        if i == 0:
+            data = pandas.read_csv(filename, delimiter=',', 
+                                   low_memory=False, index_col='time').values
+        else:
+            datatemp = pandas.read_csv(filename, delimiter=',', 
+                                   low_memory=False, index_col='time').values
+            data = numpy.dstack((data, datatemp))
+        i += 1
 
-def load_data():
-    trainingData = "learning1543692385.txt" # path to file
+    data = data.reshape(data.shape[2], data.shape[0], data.shape[1]); # sample,timestep, feature
+
+    model = Sequential()
+    model.add(LSTM(103, input_shape=(50,51)))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+
+if __name__ == "__main__":
+    main()
