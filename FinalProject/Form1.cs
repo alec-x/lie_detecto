bbda14cd9a3f5dc4ecab3f542ad63c09cb8bd936
Alec Xu <Alec_Xu@hotmail.com>
2018-12-01 00:53:42 -0800

    added helper class to C# program

--- FinalProject/Form1.cs
+++ FinalProject/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Accord.Statistics.Filters;
+using System.IO;
 
 namespace FinalProject
 {
+    
     public partial class Form1 : Form
     {
+        double[,,,] allData = new double [10, 50, 51, 2];
+        DataTable data = new DataTable("video_input");
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadVideoData();
+
+        }   
     }
 }
---
+++ FinalProject/dataLoadingHelpers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord.Statistics.Filters;
+using System.IO;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        void loadVideoData()
+        {
+            string[] coords, frames, xy;
+            string temp, contents;
+            int sampleNum = 0;
+            int frameNum = 0;
+            int coordNum = 0;
+
+            foreach (var file in Directory.EnumerateFiles("trainingData\\", "*.txt"))
+            {
+                contents = File.ReadAllText(file);
+                frames = contents.Split(';').Skip(1).ToArray();
+                foreach (var frame in frames)
+                {
+                    temp = frame.Replace('\n'.ToString(), "");
+                    coords = temp.Split('\r').Skip(1).ToArray().Reverse().Skip(1).Reverse().ToArray();
+
+                    foreach (var coord in coords)
+                    {
+                        xy = coord.Split(',');
+                        allData[sampleNum, frameNum, coordNum, 0] = Convert.ToDouble(xy[0]);
+                        allData[sampleNum, frameNum, coordNum, 1] = Convert.ToDouble(xy[1]);
+                        coordNum++;
+
+                    }
+                    coordNum = 0;
+                    frameNum++;
+                }
+                frameNum = 0;
+                sampleNum++;
+
+            }
+        }
+
+        void createDataTable()
+        {
+
+        }
+    }
+}
