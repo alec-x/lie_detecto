fbf9c3088e03c499012f35c62ab2ee8be944199c
Alec Xu <Alec_Xu@hotmail.com>
2018-12-01 17:41:28 -0800

    more teste cases

--- LSTMFaceVector/LSTMFaceVector/LSTMFaceVector.py
+++ LSTMFaceVector/LSTMFaceVector/LSTMFaceVector.py
@@ -2,14 +2,18 @@ import os
 import pandas
 import glob
 import numpy
-from pandas import DataFrame
 from pandas import concat
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Activation
+from numpy.random import seed
+from tensorflow import set_random_seed
 
 def main():
-    path = 'C:\Repos\mech423final\LSTMFaceVector\LSTMFaceVector'
+    seed(1)
+    set_random_seed(2)
+    path = 'C:\\Repos\\mech423final\\LSTMFaceVector\\trainingData'
     i = 0
+    y_data = numpy.array([])
     for filename in glob.glob(os.path.join(path, '*.txt')):
         if i == 0:
             data = pandas.read_csv(filename, delimiter=',', 
@@ -18,16 +22,25 @@ def main():
             datatemp = pandas.read_csv(filename, delimiter=',', 
                                    low_memory=False, index_col='time').values
             data = numpy.dstack((data, datatemp))
+        y_data = numpy.append(y_data, os.path.basename(filename).split("_")[0])        
         i += 1
 
-    data = data.reshape(data.shape[2], data.shape[0], data.shape[1]); # sample,timestep, feature
+    x_data = data.reshape(data.shape[2], data.shape[0], data.shape[1]); # sample,timestep, feature
     # machinelearningmastery.com/5-step-life-cycle-long-short-term-memory-models-keras/
     
     model = Sequential()
-    model.add(LSTM(256, input_shape=(50,51))) #the 256 is semi-arbitrary?
-    model.add(Dense(256)) 
+    model.add(LSTM(256, input_shape=(50,102))) #the 256 is semi-arbitrary?
+    model.add(Dense(1)) 
     model.add(Activation('sigmoid'))
     model.compile(optimizer='sgd', loss='mean_squared_error')
 
+    history = model.fit(x_data, y_data, epochs=1000, batch_size=len(x_data))
+    
+    loss = model.evaluate(x_data, y_data)
+    print(loss)
+    # 5. make predictions
+    predictions = model.predict(x_data)
+    print(y_data)
+    print(predictions[:, 0])
 if __name__ == "__main__":
     main()
